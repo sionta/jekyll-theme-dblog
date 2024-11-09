dffb81a9d60542c12f22440af9c260c948376056
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2024-11-09 14:10:45 +0900

    202411091410

--- gulpfile.js
+++ gulpfile.js
@@ -6,50 +6,51 @@ import cleanCSS from "gulp-clean-css";
 import uglify from "gulp-uglify";
 
 const paths = {
+  root: "./_site",
   html: "./_site/**/*.html",
   css: "./_site/**/*.css",
   js: "./_site/**/*.js",
 };
 
+function logCompletion(taskName) {
+  console.log(`Task ${taskName} completed successfully.`);
+}
+
 gulp.task("minify-html", () => {
   return gulp
     .src(paths.html)
-    .pipe(plumber())
+    .pipe(plumber({ errorHandler: (err) => console.error(err) }))
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("./_site"));
+    .pipe(gulp.dest(paths.root))
+    .on("end", () => logCompletion("minify-html"));
 });
 
 gulp.task("minify-css", () => {
   return gulp
     .src(paths.css)
-    .pipe(plumber())
+    .pipe(plumber({ errorHandler: (err) => console.error(err) }))
     .pipe(cleanCSS({ compatibility: "ie8" }))
     .pipe(
       gulp.dest((file) => {
-        const relativePath = path.relative(path.resolve("./_site"), file.base);
-        return path.join("./_site", relativePath);
+        const relativePath = path.relative(path.resolve(paths.root), file.base);
+        return path.join(paths.root, relativePath);
       })
     )
-    .on("end", () => {
-      console.log(`Minified CSS processed.`);
-    });
+    .on("end", () => logCompletion("minify-css"));
 });
 
 gulp.task("minify-js", () => {
   return gulp
     .src(paths.js)
-    .pipe(plumber())
+    .pipe(plumber({ errorHandler: (err) => console.error(err) }))
     .pipe(uglify())
     .pipe(
       gulp.dest((file) => {
-        const relativePath = path.relative(path.resolve("./_site"), file.base);
-        return path.join("./_site", relativePath);
+        const relativePath = path.relative(path.resolve(paths.root), file.base);
+        return path.join(paths.root, relativePath);
       })
     )
-    .on("end", () => {
-      console.log(`Minified JS processed.`);
-    });
+    .on("end", () => logCompletion("minify-js"));
 });
 
-// Default task without image optimization
 gulp.task("default", gulp.series("minify-html", "minify-css", "minify-js"));
